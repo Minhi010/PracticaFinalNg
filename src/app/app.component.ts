768fccd9c260ce75f1a011cc9612ce3a21b033a5
Minhi010 <lconlyentretenim@gmail.com>
2022-09-29 22:33:47 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'admng-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'practicaFinalng';
+}
